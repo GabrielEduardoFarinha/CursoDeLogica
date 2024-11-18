0f67073d2917663e63ffc04177fba56cb4a7c162
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2024-11-18 15:13:49 -0300

    commit da aula 3
    commit da nova função de teste se o numero chutado for maior ou menor que o numero correto.

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 alert('bem vindo ao jogo');
 let numeroSecreto = 9;
-console.log(numeroSecreto)
-let chute = prompt('escolha um numero entre 1 e 30');
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-// acerto
-if(numeroSecreto == chute){
-    alert('acertouuuuu' + numeroSecreto);
-} else {
-    alert('errouuu :(')
+// testando
+while(chute != numeroSecreto) {
+    chute = prompt('escolha um numero entre 1 e 30');
+    // acerto
+    if(chute == numeroSecreto){
+        alert('acertouuuuu ' + numeroSecreto + ' com ' + tentativas + ' tentativas ');
+    } else {
+        if(chute > numeroSecreto) {
+            alert('o numero secreto é menor que ' + chute);
+            } else{
+                alert('o numero secreto é maior que ' + chute);
+            }
+            tentativas++;
+    }
 }
